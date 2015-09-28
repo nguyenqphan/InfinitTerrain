492d2f7dfe0bac3e0add74f1aa6adec5dc51174f
Nguyen Phan <nqphan212@gmail.com>
2015-09-28 18:23:05 -0400

    Redraw the map to create an infinite terrain

--- Assets/Scripts/TerrainManager.cs
+++ Assets/Scripts/TerrainManager.cs
@@ -2,34 +2,52 @@
 using System.Collections;
 
 public class TerrainManager : MonoBehaviour {
-
 	public Sprite[] Sprites;
 	public int HorizontalTiles = 25;
-	public int VerticaleTiles = 25;
+	public int VerticalTiles = 25;
 	public int Key = 1;
-
-	public Sprite SelecctRandomSprite(int x, int y){
-		return Sprites [RandomHelper.Range(x, y, Key, Sprites.Length)];
-	}
+	public Transform Player;
+	public float MaxDistanceFromCenter = 7;
 	
-
-	// Use this for initialization
-	void Start () {
-		var offset = new Vector3 (0 - HorizontalTiles / 2, 0 - VerticaleTiles / 2, 0);
-		for(int x = 0; x < HorizontalTiles; x++){
-			for(int y = 0; y < VerticaleTiles; y++){
-				var tile = new GameObject();
-				tile.transform.position = new Vector3(x, y, 0) + offset;
-				var spriteRenderer = tile.AddComponent<SpriteRenderer>();
-				spriteRenderer.sprite = SelecctRandomSprite(x, y);
-				tile.name = "Terrain " + tile.transform.position;
-				tile.transform.parent = transform;
+	private SpriteRenderer[,] _renderers;
+	
+	public Sprite SelectRandomSprite(int x, int y)
+	{
+		return Sprites [RandomHelper.Range (x, y, Key, Sprites.Length)];
+	}
+	void RedrawMap()
+	{
+		transform.position = new Vector3((int)Player.position.x, (int)Player.position.y, Player.position.z);
+		for (int x = 0; x < HorizontalTiles; x++) {
+			for (int y = 0; y < VerticalTiles; y++) {
+				var spriteRenderer = _renderers[x,y];
+				spriteRenderer.sprite = SelectRandomSprite(
+					(int)transform.position.x + x,
+					(int)transform.position.y + y);
 			}
 		}
 	}
 	
+	// Use this for initialization
+	void Start () {
+		var offset = new Vector3 (0 - HorizontalTiles / 2, 0 - VerticalTiles / 2, 0);
+		_renderers = new SpriteRenderer[HorizontalTiles,VerticalTiles];
+		for (int x = 0; x < HorizontalTiles; x++) {
+			for (int y = 0; y < VerticalTiles; y++) {
+				var tile = new GameObject();
+				tile.transform.position = new Vector3(x, y, 0) + offset;
+				_renderers[x,y] = tile.AddComponent<SpriteRenderer>();
+				tile.name = "Terrain " + tile.transform.position;
+				tile.transform.parent = transform;
+			}
+		}
+		RedrawMap ();
+	}
+	
 	// Update is called once per frame
 	void Update () {
-	
+		if (MaxDistanceFromCenter < Vector3.Distance (Player.position, transform.position)) {
+			RedrawMap ();
+		}
 	}
 }
