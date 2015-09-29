69be0a3f1a7be8196cac6f7012c803807d3747f1
Nguyen Phan <nqphan212@gmail.com>
2015-09-29 12:11:21 -0400

    Add a player to walk around

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -5,6 +5,9 @@ public class Move : MonoBehaviour {
 	public float Speed = 1;
 	public float FastSpeed = 3;
 	public KeyCode EnableFastSpeedWithKey = KeyCode.LeftShift;
+	public Transform TurnWithMovement;
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -18,5 +21,10 @@ public class Move : MonoBehaviour {
 		}
 		var movement = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
 		transform.Translate (movement * currentSpeed * Time.deltaTime);
+
+		//turn the player to the direction that it is heading.
+		if (movement.x > 0.1f || movement.x < -0.1f || movement.y > 0.1f || movement.y < -0.1f) {
+			TurnWithMovement.rotation = Quaternion.LookRotation (Vector3.back, movement.normalized);
+		}
 	}
 }
